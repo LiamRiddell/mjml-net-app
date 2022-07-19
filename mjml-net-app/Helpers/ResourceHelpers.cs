e02fa195aa022343e663c86078ca126ac30215d1
Liam Riddell <web@liamriddell.co.uk>
2022-07-19 20:05:14 +0100

    Added Embedded Resource Loader

---
+++ mjml-net-app/Helpers/ResourceHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mjml.Net.App.Helpers
+{
+    public static class ResourceHelpers
+    {
+        public static async Task<string> ReadResourceAsync(string name)
+        {
+            // Determine path
+            var assembly = Assembly.GetExecutingAssembly();
+            string resourcePath = name;
+
+            // Format: "{Namespace}.{Folder}.{filename}.{Extension}"
+            resourcePath = assembly.GetManifestResourceNames().Single(str => str.EndsWith(name));
+            
+            using (Stream stream = assembly.GetManifestResourceStream(resourcePath))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
--- mjml-net-app/MainPage.xaml.cs
+++ mjml-net-app/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Web.WebView2.Core;
+using Mjml.Net.App.Helpers;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -50,60 +51,9 @@ namespace Mjml.Net.App
         {
             webviewMonacoEditor.CoreWebView2.OpenDevToolsWindow();
 
-            webviewMonacoEditor.NavigateToString(@"
-                <!DOCTYPE html>
-                <html lang=""en"">
+            var htmlTemplateEditorPage = await ResourceHelpers.ReadResourceAsync("Mjml.Net.App.Resources.TemplateEditorPage.html");
 
-                <head>
-                    <meta charset=""UTF-8"">
-                    <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"">
-                    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
-                    <title>mjml-net-app</title>
-                    <style>
-                        html,
-                        body,
-                        #container {
-                            position: absolute;
-                            left: 0;
-                            top: 0;
-                            width: 100%;
-                            height: 100%;
-                            margin: 0;
-                            padding: 0;
-                            overflow: hidden;
-                        }
-                    </style>
-                </head>
-
-                <body>
-                    <script src=""https://unpkg.com/monaco-editor@latest/min/vs/loader.js""></script>
-                    <div id=""container""></div>
-                    <script>
-                        require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' } });
-                        window.MonacoEnvironment = { getWorkerUrl: () => proxy };
-
-                        let proxy = URL.createObjectURL(new Blob([`
-                            self.MonacoEnvironment = {
-                                baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
-                            };
-                            importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
-                        `], { type: 'text/javascript' }));
-
-                        require([""vs/editor/editor.main""], function () {
-                            let editor = monaco.editor.create(document.getElementById('container'), {
-                                value: [
-                                    'function x() {',
-                                    '\tconsole.log(""Hello world!"");',
-                                    '}'
-                                ].join('\n'),
-                                language: 'javascript',
-                                theme: 'vs-dark'
-                            });
-                        });
-                    </script>
-                </body>
-                </html>
-            ");
+            webviewMonacoEditor.NavigateToString(htmlTemplateEditorPage);
         }
 
         private async void webviewPreview_CoreWebView2Initialized(Microsoft.UI.Xaml.Controls.WebView2 sender, Microsoft.UI.Xaml.Controls.CoreWebView2InitializedEventArgs args)
