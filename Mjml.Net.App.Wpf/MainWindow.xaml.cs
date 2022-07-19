4853a837863ad8845f7e655cf9c0e764b72b4cc5
Liam Riddell <web@liamriddell.co.uk>
2022-07-19 22:38:33 +0100

    PoC

--- Mjml.Net.App.Wpf/MainWindow.xaml.cs
+++ Mjml.Net.App.Wpf/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Mjml.Net.App.Wpf.Helpers;
 using Mjml.Net.App.Wpf.HostObjects;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 
@@ -11,11 +13,29 @@ namespace Mjml.Net.App.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MjmlRenderer MjmlRenderer { get; set; }
+        public string DefaultMjmlTemplateString { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            MjmlRenderer = new MjmlRenderer();
+            DefaultMjmlTemplateString = @"
+<mjml>
+   <mj-body>
+       <mj-section>
+           <mj-column>
+               <mj-image width=""100px"" src=""https://mjml.io/assets/img/logo-small.png""></mj-image>
+               <mj-divider border-color=""#F45E43""></mj-divider>
+               <mj-text font-size=""20px"" color=""#F45E43"" font-family=""helvetica"">Hello World</mj-text>
+           </mj-column>
+       </mj-section>
+   </mj-body>
+</mjml>    
+            ";
         }
 
+    
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
             await webviewMonacoEditor.EnsureCoreWebView2Async();
@@ -40,12 +60,46 @@ namespace Mjml.Net.App.Wpf
         }
         private async void webviewPreview_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
         {
+            // DefaultMjmlTemplateString
+            #if DEBUG
+            webviewPreview.CoreWebView2.OpenDevToolsWindow();
+            #endif
 
+            // Compile the default template
+            if (TryCompileMjmlTemplateAsync(DefaultMjmlTemplateString, out string htmlContent))
+            {
+                webviewPreview.NavigateToString(htmlContent);
+            }
         }
 
-        private void TextEditorBridgeHostObject_OnDidChangeContent(string modelContent)
+        private async void TextEditorBridgeHostObject_OnDidChangeContent(string modelContent)
         {
             Trace.WriteLine($"[TextEditor_OnDidChangeContent] OnEditorChangeDetected: {modelContent}");
-        }      
+
+            if (TryCompileMjmlTemplateAsync(modelContent, out string htmlContent))
+            {
+                webviewPreview.NavigateToString(htmlContent);
+            }
+        } 
+        
+        private bool TryCompileMjmlTemplateAsync(string mjmlTemplate, out string htmlContent)
+        {
+            htmlContent = string.Empty;
+
+            if (MjmlRenderer == null)
+                return false;
+
+            mjmlTemplate = MjmlRenderer.FixXML(mjmlTemplate);
+
+            var result = MjmlRenderer.Render(mjmlTemplate);
+
+            if (result.Errors.Any())
+            {
+                return false;
+            }
+
+            htmlContent = result.Html;
+            return true;
+        }
     }
 }
