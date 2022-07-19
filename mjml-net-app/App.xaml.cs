cb864080766807d3b73eb597eea73a42f888b7b5
Liam Riddell <web@liamriddell.co.uk>
2022-07-19 19:23:27 +0100

    Added Webview and Monaco Editor

--- mjml-net-app/App.xaml.cs
+++ mjml-net-app/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace mjml_net_app
+namespace Mjml.Net.App
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
